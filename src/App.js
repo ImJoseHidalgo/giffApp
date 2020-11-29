d901c58cab4265cf0648b9ecdbfdb7031378391f
jhidalgo990 <josehidalgo990@gmail.com>
2020-11-29 19:56:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const apiURL =
+  "https://api.giphy.com/v1/gifs/search?api_key=BYKsFzfenvLYOR9gbpPxcHDFvxTZ2C7W&q=&limit=25&offset=0&rating=g&lang=en";
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    console.log("actual");
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        const { data } = response;
+        console.log(data);
+        const gifs = data.map((images) => images.images.downsized_small.url);
+        setGifs(gifs);
+        console.log(gifs);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {gifs.map((singleGif) => (
+          <img src={singleGif}></img>
+        ))}
+      </section>
     </div>
   );
 }
