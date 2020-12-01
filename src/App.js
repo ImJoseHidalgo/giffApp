419e6538d2c5e6f3ac8a25fd403a46ddc3aa1c85
jhidalgo990 <josehidalgo990@gmail.com>
2020-11-30 22:59:30 -0400

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
+import { Link, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
+      <div className="links">
+        <h1>App</h1>
+        <Link to="/gif/panda">Pandas</Link>
+        <Link to="/gif/code">Code</Link>
+        <Link to="/gif/Stewie-Griffin">Stewie</Link>
+        <Link to="/gif/Kyle-South-Park">Kyle</Link>
+      </div>
       <section className="App-content">
-        <ListOfGifs keyword="kyle broflovski" />
+        <Route path="/gif/:keyword" component={ListOfGifs} />
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-function ListOfGifs({ keyword }) {
+function ListOfGifs({ params }) {
+  const { keyword } = params;
+  const [loading, setLoading] = useState(false);
   const [gifs, setGifs] = useState([]);
 
   useEffect(
     function () {
-      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+      setLoading(true);
+      getGifs({ keyword }).then((gifs) => {
+        setGifs(gifs);
+        setLoading(false);
+      });
     },
     [keyword]
   );
 
-  return gifs.map(({ id, title, url }) => (
-    <Gif id={id} key={id} title={title} url={url} />
-  ));
+  if (loading) return <span className="loading"> Cargando...</span>;
+
+  return (
+    <div className="khe">
+      {gifs.map(({ id, title, url }) => (
+        <Gif id={id} key={id} title={title} url={url} />
+      ))}
+    </div>
+  );
 }
 export default ListOfGifs;
